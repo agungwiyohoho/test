030d8e21013e51c349a2e35d133f52907cf2c882
agungwiyohoho <57871163+agungwiyohoho@users.noreply.github.com>
2020-04-13 21:37:56 +0700

    Update function.php

--- function.php
+++ function.php
@@ -7,11 +7,11 @@ $header[] = "User-Agent: okhttp/3.10.0";
 $header[] = "Accept: application/json";
 $header[] = "Accept-Language: id-ID";
 $header[] = "Content-Type: application/json; charset=UTF-8";
-$header[] = "X-AppVersion: 3.30.2";
+$header[] = "X-AppVersion: 3.50.1";
 $header[] = "X-UniqueId: ".time()."57".mt_rand(1000,9999);
 $header[] = "Connection: keep-alive";
 $header[] = "X-User-Locale: id_ID";
-$header[] = "X-Location: -7.632692,111.537323";
+$header[] = "X-Location: -7.473772,112.430649";
 $header[] = "X-Location-Accuracy: 3.0";
 if ($pin):
 $header[] = "pin: $pin";
